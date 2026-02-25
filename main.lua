a83fc13c16d6dc856426a9be987d59660c239aec
Saterz <tilouc93@gmail.com>
2026-02-25 14:54:41 +0200

    fix: ssid string and dynamic menu loading

--- main.lua
+++ main.lua
@@ -12,6 +12,10 @@ local _ = require("gettext")
 
 local networks = {}
 
+local function refresh_nearby_networks()
+	networks = NetworkMgr:getNetworkList() or {}
+end
+
 local BetterWifi = WidgetContainer:extend{
     name = "betterwifi",
     is_doc_only = false,
@@ -21,17 +25,13 @@ function BetterWifi:init()
     self.ui.menu:registerToMainMenu(self)
 end
 
-local function refresh_nearby_networks()
-	networks = NetworkMgr:getNetworkList() or {}
-end
-
 local function nearby_list_submenu()
 	refresh_nearby_networks()
 
 	local items = {}
 	for _, network in ipairs(networks) do
 		table.insert(items, {
-			text = _(network.ssid) or _("Hidden SSID")
+			text = network.ssid or _("Hidden SSID")
 		})
 	end
 
@@ -43,25 +43,28 @@ function BetterWifi:addToMainMenu(menu_items)
         text = _("Better WiFi"),
         -- in which menu this should be appended
         sorting_hint = "setting",
-
-	sub_item_table = {
-		{
-			text = _("Activate"),
-			checked_func = function()
-				return NetworkMgr:isWifiOn()
-			end,
-			callback = function()
-				NetworkMgr:enableWifi(function()
-					networks = NetworkMgr:getNetworkList()
-				end, true)
-			end,
-		},
-		{
-			text = _("Nearby networks"),
-			sub_item_table = nearby_list_submenu()
-		}
-	},
-}
+        sub_item_table = {
+          {
+            text = _("Activate"),
+            checked_func = function()
+              return NetworkMgr:isWifiOn()
+            end,
+            callback = function()
+              if NetworkMgr:isWifiOn() then
+                NetworkMgr:disableWifi()
+              else
+              	 NetworkMgr:enableWifi()
+              end
+            end,
+          },
+          {
+            text = _("Nearby networks"),
+            sub_item_table_func = function()
+            	 return nearby_list_submenu()
+            end
+          }
+        },
+    }
 end
 
 return BetterWifi
