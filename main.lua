2075ff10ee5225cf3d2d0b2e96d4788b0e776f51
Saterz <tilouc93@gmail.com>
2026-02-25 12:46:18 +0200

    feat: add basic network toggle and nearby list

--- main.lua
+++ main.lua
@@ -7,7 +7,10 @@ This is a debug plugin to test Plugin functionality.
 local InfoMessage = require("ui/widget/infomessage")
 local UIManager = require("ui/uimanager")
 local WidgetContainer = require("ui/widget/container/widgetcontainer")
+local NetworkMgr = require("ui/network/manager")
 local _ = require("gettext")
+
+local networks = {}
 
 local BetterWifi = WidgetContainer:extend{
     name = "betterwifi",
@@ -18,6 +21,23 @@ function BetterWifi:init()
     self.ui.menu:registerToMainMenu(self)
 end
 
+local function refresh_nearby_networks()
+	networks = NetworkMgr:getNetworkList() or {}
+end
+
+local function nearby_list_submenu()
+	refresh_nearby_networks()
+
+	local items = {}
+	for _, network in ipairs(networks) do
+		table.insert(items, {
+			text = _(network.ssid) or _("Hidden SSID")
+		})
+	end
+
+	return items
+end
+
 function BetterWifi:addToMainMenu(menu_items)
     menu_items.betterwifi = {
         text = _("Better WiFi"),
@@ -25,16 +45,23 @@ function BetterWifi:addToMainMenu(menu_items)
         sorting_hint = "setting",
 
 	sub_item_table = {
-	  {
-	    text = _("Activate"),
-	    callback = function()
-	      UIManager:show(
-		InfoMessage:new{text=_("Test")}
-	      )
-	    end,
-	  }
+		{
+			text = _("Activate"),
+			checked_func = function()
+				return NetworkMgr:isWifiOn()
+			end,
+			callback = function()
+				NetworkMgr:enableWifi(function()
+					networks = NetworkMgr:getNetworkList()
+				end, true)
+			end,
+		},
+		{
+			text = _("Nearby networks"),
+			sub_item_table = nearby_list_submenu()
+		}
 	},
-    }
+}
 end
 
 return BetterWifi
